257225b4d468269563a81ef217c4a0bc1ea77c50
Madhura Yuvaraj Badhe <75791284+Madhura247@users.noreply.github.com>
2021-01-29 15:18:23 +0530

    Update drops.js

--- drops.js
+++ drops.js
@@ -1,33 +1,27 @@
 class Drops {
     constructor(x,y, r) {
       var options = {
-          "friction" : 0.1,
-      }
-      
+          friction : 0.1,
+      }  
+      this.body = Bodies.circle(x,y,r, options);
       this.x = x;
       this.y = y;
       this.r = r;
-
-      this.body = Bodies.circle(x,y,r,options);
-
-      if(this.body.position.y > height) {
-         Matter.Body.setPosition(this.body, {x:random(0, 400), y:random(0,400)})
-      }
-      
       World.add(world, this.body);
     }
 
     display(){
-      var pos =this.body.position;
+      var pos = this.body.position;
       push();
       translate(pos.x, pos.y)
       ellipseMode(RADIUS);
       fill("blue");
       ellipse(pos.x, pos.y, this.r, this.r);
-      var maxDrops=100;
-      for(var i=0; i<maxDrops; i++) {
-          this.push(new createDrop(random(0, 400), random(0, 400)));
-      }
+
+      if(this.body.position.y > height) {
+        Matter.Body.setPosition(this.body, {x:random(0, 400), y:random(0,400)})
+     }
+
       pop();
     }
   }
